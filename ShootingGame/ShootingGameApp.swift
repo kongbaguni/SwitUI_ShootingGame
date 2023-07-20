80e1241b4cb61463efb09696714e5e0e48c718d7
서창열 <kongbaguni@gmail.com>
2023-07-20 14:02:26 +0900

    Initial Commit

---
+++ ShootingGame/ShootingGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShootingGameApp.swift
+//  ShootingGame
+//
+//  Created by Changyeol Seo on 2023/07/20.
+//
+
+import SwiftUI
+
+@main
+struct ShootingGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
